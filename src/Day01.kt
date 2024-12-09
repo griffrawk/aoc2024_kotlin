cf92a1b1afe5155ed2b5be3e739f51a1fe26f91e
Andy Griffiths <griffiths_andy@icloud.com>
2024-12-09 21:34:00 +0000

    1.1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,32 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val s1 = mutableListOf<Int>()
+        val s2 = mutableListOf<Int>()
+        input.forEach { x ->
+            x.split(Regex("\\s+")) // Split the line by whitespace
+                .map(String::toInt) // Convert strings to integers
+                .let { (e1, e2) -> s1.add(e1); s2.add(e2) } // Add to respective lists
+        }
+
+        s1.sort() // Sort the first list
+        s2.sort() // Sort the second list
+//        return (s1 zip s2).fold(0) { sum, (a, b) -> sum + (a - b).absoluteValue }
+        return (s1 zip s2).sumOf { (a, b) -> (a - b).absoluteValue }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
